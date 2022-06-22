2b97e9cff49e8183faa57c022a58b32dc50bd6ad
Bagsj <stepanuyk92@gmail.com>
2022-06-22 22:18:16 +0300

    slider kek

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(function(){
   $('.feedback__slider').slick({
-      arrows: false,
-      dots: true,
+      arrows: true,
+      dots: false,
       fade: true,
       autoplay: true,
       autoplaySpeed: 2000,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      centerMode: true,
   })  
 })
